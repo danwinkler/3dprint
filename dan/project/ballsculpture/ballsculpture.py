7a65330c41753f30f03c8f2fc30b32496b94bc63
Daniel Winkler <dan@danwink.com>
2014-06-29 18:57:50 -0500

    ballsculpture

--- dan/project/ballsculpture/ballsculpture.py
+++ dan/project/ballsculpture/ballsculpture.py
@@ -12,6 +12,8 @@ balls = []
 
 balls.append( [Vec3(0,0,0), 20] )
 
+height = 50
+
 while True:
 	lb = balls[random.randint(0,len(balls)-1)]
 	v = Vec3( random.uniform(-1,1), random.uniform(-1,1), random.uniform(1,1) )
@@ -25,7 +27,7 @@ while True:
 	
 	v += lb[0]
 	
-	if v.z > 100:
+	if v.z > height:
 		continue
 	
 	good = True
@@ -36,16 +38,24 @@ while True:
 	
 	if good:
 		balls.append( [v, s] )
-		print "added"
+		print "added " + str(len(balls))
 	
-	if len(balls) > 100:
+	if len(balls) >= 70:
 		break
 
 parts = []
 
+neg_parts = []
+
+neg_parts.append( translate( [-500,-500,-1000] ) ( cube( 1000 ) ) )
+
 for ball in balls:
-	parts.append( translate( ball[0].to_list() ) ( sphere( ball[1] ) ) )
+	ball[0].z = height - ball[0].z
+	parts.append( translate( ball[0].to_list() ) ( sphere( ball[1], segments=int(ball[1]*.5)+9 ) ) )
+	#neg_parts.append( translate( ball[0].to_list() ) ( sphere( ball[1]-2 ) ) )
+	
+
 
 print "Saving File"
 with open( __file__ + ".scad", "w" ) as f:
-	f.write( scad_render( union()( parts ) - translate( [-500,-500,-1000] ) ( cube( 1000 ) ) ) )
+	f.write( scad_render( union()( parts ) - union()( neg_parts ) ) )
