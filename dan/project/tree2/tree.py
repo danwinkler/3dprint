f76b626b0c81b3c862f914ec9bdb7fdda18fd3a3
Daniel Winkler <dan@danwink.com>
2014-07-05 19:24:33 -0500

    changes to tree2
    not really a tree anymore

--- dan/project/tree2/tree.py
+++ dan/project/tree2/tree.py
@@ -20,14 +20,33 @@ def r_vine():
 	variance = .3
 	z_var = .1
 	length = 4
+	height = 50
 
 	vec = Vec3( 0, 0, 1 )
-	for i in range( 100 ):
+	for i in range( height ):
 		vec.normalize()
 		
-		vec.x += random.uniform( -variance, variance ) + ((-vec.x ** 3) * 1)
-		vec.y += random.uniform( -variance, variance ) + ((-vec.y ** 3) * 1)
-		vec.z += random.uniform( -z_var, z_var )
+		lp = points[-1]
+		
+		dx = 0
+		mx = 0
+		if lp.x != 0:		
+			dx = lp.x + 20
+			mx = ((dx**2) * i**2 * .000001) * (-1 if dx > 0 else 1)
+		
+		dy = 0
+		my = 0
+		if lp.y != 0:
+			dy = lp.y
+			my = ((dy**2) * .001) * (-1 if dy > 0 else 1)
+		
+		
+		vec.x += random.uniform( -variance, variance ) + mx
+		vec.y += random.uniform( -variance, variance ) + my
+		#vec.z += random.uniform( -z_var, z_var )
+		vec.z += .1
+		
+		vec.normalize()
 		
 		vec *= length
 		
@@ -40,10 +59,13 @@ def r_vine():
 	
 	return vine( points, wf )
 
-for i in range( 10 ):
-	parts.append( r_vine() )
-	
-	
+vines = 20
+for i in range( vines ):
+	angle = (i / float(vines)) * 360
+	parts.append( rotate( angle ) ( translate( [20,0,0] ) ( r_vine() ) ) )
+
+parts.append( down( 2 ) ( cylinder( r=25, h = 4 ) ) )	
+
 print "Saving File"
 with open( __file__ + ".scad", "w" ) as f:
 	f.write( scad_render( union()( parts ) ) )
