baa2d0f15cf6a13ff0217f8ee2bf190bee5016cc
Daniel Winkler <dan@danwink.com>
2015-04-29 18:22:18 -0500

    deck

--- dan/project/templ/templ.py
+++ dan/project/templ/templ.py
@@ -4,13 +4,11 @@ sys.path.append( "../../../" )
 import math
 import random
 
-from solid import *
-from solid.utils import *
+from dan.lib.traceprint_python import *
 
 parts = []
 
-parts.append( cylinder( r=10, h=40 ) )
+parts.append( cylinder( 10, 40 ) )
 
 print "Saving File"
-with open( __file__ + ".scad", "w" ) as f:
-	f.write( scad_render( union()( parts ) ) )
+write_out( __file__ + ".json", union( *parts ) )
