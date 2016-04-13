428c324224dc3f9d823d2707d2245ed82cd36395
Daniel Winkler <dan@danwink.com>
2016-04-13 18:37:15 -0400

    starting on connector function

--- dan/project/rod_connector/connector.py
+++ dan/project/rod_connector/connector.py
@@ -17,18 +17,54 @@ half_in = inches_in_mm / 2.0
 parts = []
 neg_parts = []
 
-#parts.append( cube( [20, 20, 5] ) )
-
 rad = half_in+.5
 outer_rad = rad + 5
 rad = rad / 2.0
 outer_rad = outer_rad/2.0
 
+def connector( vec_list, size_in_inches=half_in ):
+	size = size_in_inches+.5
+	rad = size / 2.0
+
+	avg = Vec3()
+	up_vec = Vec3( 0, 0, 1 )
+
+	for v in vec_list:
+		v.normalize()
+		avg += v
+
+	avg.normalize()
+
+	a_rot_vec = up_vec.cross( avg )
+	a_rot_angle = math.acos( up_vec.dot( avg ) )
+
+	part = sphere( 30 )
+
+	for v in vec_list:
+		cross = up_vec.cross( v )
+		angle = math.acos( up_vec.dot( v ) )
+		part -= rotate( a=-math.degrees( a_rot_angle ), v=a_rot_vec.to_list() ) (
+			rotate( a=math.degrees(angle), v=cross.to_list() ) (
+				up( 10 ) ( cylinder( r=rad, h=30 ) )
+			)
+		)
+
+	return part
+
+parts.append( connector([
+	Vec3( 0, 0, 1 ),
+	Vec3( 1, 0, 1 ),
+	Vec3( 0, -1, 1 ),
+	Vec3( 0, 1, 0 ),
+	Vec3( 1, 0, 0 )
+] ) )
+
+'''
 def port():
 	return translate( [0, 0, 0] ) (
 		cylinder(r=outer_rad, h=25)
 	)
-	
+
 def port_neg():
 	return translate( [0, 0, 0] ) (
 		up(5)(cylinder(r=rad, h=30)),
@@ -39,15 +75,15 @@ def make_port( a, v, r=0 ):
 	p = [port()]
 	if a == 90 or a == -90:
 		p.append( translate( [-outer_rad, -outer_rad, 0] ) ( cube( [outer_rad*2, outer_rad*2, 25] ) ) )
-	parts.append( 
+	parts.append(
 		rotate( a=a, v=v ) (
 			up( 15 ) (
 				p
 			)
 		)
 	)
-	
-	neg_parts.append( 
+
+	neg_parts.append(
 		rotate( a=a, v=v ) (
 			up( 15 ) (
 				rotate( a=r, v=[0,0,1] ) (
@@ -65,7 +101,8 @@ make_port( -60, [1, 0, 0], r=-90 )
 make_port( -90, [0, 1, 0] )
 
 parts.append( down( outer_rad ) ( cylinder( r=20, h=outer_rad ) ) )
+'''
 
 print "Saving File"
 with open( __file__ + ".scad", "w" ) as f:
-	f.write( scad_render( union()( parts ) - union() ( neg_parts ) ) )
+	f.write( scad_render( union()( parts ) ) )
