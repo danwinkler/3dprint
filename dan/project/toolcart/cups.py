c3c0eade531d9cc449a9004b77d65d51abfe8e9a
Daniel Winkler <dan@danwink.com>
2016-09-11 15:23:12 -0500

    cups and rims

---
+++ dan/project/toolcart/cups.py
@@ -0,0 +1,56 @@
+import sys
+sys.path.append( "../../../" )
+
+from dan.lib.helper import *
+from dan.lib.premade import *
+
+import math
+import random
+
+from solid import *
+from solid.utils import *
+
+mm_in_inches = 25.4
+wall_thickness = 2
+rim_width = 8
+
+def rim( diameter, depth ):
+    rad = diameter*.5
+    return union() (
+        cylinder( r=rad+rim_width, h=wall_thickness, segments=60 ),
+        cylinder( r=rad, h=depth+wall_thickness, segments=60 ),
+        hole() (
+            down( 1 ) (
+                cylinder( r=rad-wall_thickness, h=depth+wall_thickness+2, segments=60 )
+            )
+        )
+    )
+
+def cup( diameter, depth ):
+    rad = diameter*.5
+    wall_height = max( depth-rad, 0 )
+    return union() (
+        cylinder( r=rad+rim_width, h=wall_thickness, segments=60 ),
+        cylinder( r=rad, h=wall_height+wall_thickness, segments=60 ),
+        up( wall_height+wall_thickness ) (
+            sphere( r=rad, segments=60 )
+        ),
+        hole() (
+            down( 1 ) (
+                cylinder( r=rad-wall_thickness, h=wall_height+wall_thickness+1 )
+            ),
+            up( wall_height+wall_thickness ) (
+                sphere( r=rad-wall_thickness, segments=60 )
+            )
+        )
+    )
+
+
+def save( filename, thing ):
+    print "Saving File " + filename
+    with open( filename + ".scad", "w" ) as f:
+        f.write( scad_render( union() ( thing ) ) )
+
+save( "rim2", rim( mm_in_inches*2, mm_in_inches*.75 + 1 ) )
+save( "rim1.5", rim( mm_in_inches*1.5, mm_in_inches*.75 + 1 ) )
+save( "cup2_3", cup( mm_in_inches*2, mm_in_inches*3 ) )
