78a6cf730c73e3b2aee5cb8a0bc18759fdc75276
Daniel Winkler <dan@danwink.com>
2016-09-12 21:20:40 -0500

    front lip brackets

---
+++ dan/project/toolcart/frontlipbracket.py
@@ -0,0 +1,79 @@
+import sys
+sys.path.append( "../../../" )
+
+from dan.lib.helper import *
+from dan.lib.premade import *
+
+import math
+import random
+
+from solid import *
+from solid.utils import *
+
+wall_thickness = 4
+
+inner_x = 19
+inner_y = 38
+
+length = 40
+side_length = 40
+
+outer_x = inner_x + wall_thickness*2
+outer_y = inner_y + wall_thickness*2
+
+wood_lip_extra = 6
+angle_offset = 20
+angle = -36.87
+
+parts = []
+
+side_wall_base = cube( [outer_x+side_length, outer_y, wall_thickness] )
+
+front_wall = up( wall_thickness ) (
+    cube( [wall_thickness, outer_y, length])
+)
+
+side_wall = translate( [outer_x-wall_thickness, 0, wall_thickness] ) (
+    cube( [side_length+wall_thickness, outer_y, wood_lip_extra] )
+)
+
+floor = up( wall_thickness ) (
+    cube( [outer_x+side_length, wall_thickness, length] )
+)
+
+cut = hole() (
+    translate( [outer_x+angle_offset, 0, 0] ) (
+        rotate( a=angle, v=[0,0,1] ) (
+            cube( [50, 50, 50] )
+        )
+    )
+)
+
+holes = hole() (
+    translate( [0, wall_thickness+inner_x*.5, wall_thickness+length*.5] ) (
+        rotate( v=[0, 1, 0], a=-90 ) (
+            screwhole( "#8 Wood", wall_thickness+1 )
+        )
+    ),
+    translate( [outer_x*.5, 0, outer_y*.5-5] ) (
+        rotate( v=[1, 0, 0], a=90 ) (
+            screwhole( "#8 Wood", wall_thickness+1 )
+        )
+    )
+)
+
+parts += [
+    side_wall_base,
+    front_wall,
+    side_wall,
+    floor,
+    cut,
+    holes
+]
+
+print "Saving File"
+with open( __file__ + ".scad", "w" ) as f:
+    f.write( scad_render( union() ( parts ) ) )
+
+with open( __file__ + ".flipped.scad", "w" ) as f:
+    f.write( scad_render( scale( [-1, 1, 1] ) ( union() ( parts ) ) ) )
