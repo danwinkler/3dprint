0cf4ce612cd7719d6bc7cd82da80317d617ab1a4
Daniel Winkler <dan@danwink.com>
2017-01-03 20:38:37 -0600

    voronoi example

---
+++ dan/project/alg_tests/voronoi.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append( "../../../" )
+
+from dan.lib.helper import *
+
+import math
+import random
+
+from solid import *
+from solid.utils import *
+
+from scipy.spatial import Voronoi
+
+points = [[random.uniform(0, 10), random.uniform(0, 10)] for i in range(100)]
+
+vor = Voronoi( points )
+
+#Remove incomplete regions
+regions = filter(lambda x: all(i >= 0 for i in x) and len(x) > 0, vor.regions)
+
+#Assemble extruded sections
+parts = []
+for region in regions:
+    h = random.uniform(.5, 1.5)
+
+    verts = [vor.vertices[r] for r in region]
+    parts.append(
+        linear_extrude( height=h ) (
+            polygon( points=[[v[0], v[1]] for v in verts] )
+        )
+    )
+
+print "Saving File"
+with open( __file__ + ".scad", "w" ) as f:
+    f.write( scad_render( union() ( parts ) ) )
