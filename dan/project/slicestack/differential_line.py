31cd390a3a385416c8a1c99b5c19631998ba0b8f
Daniel Winkler <dan@danwink.com>
2018-10-04 21:58:03 -0700

    using nested rings in simulation

--- dan/project/slicestack/differential_line.py
+++ dan/project/slicestack/differential_line.py
@@ -1,5 +1,8 @@
 import math
 import random
+from collections import namedtuple
+
+from numba import jit
 
 from dan.lib.helper import Vec3
 
@@ -17,19 +20,37 @@ class NodeData:
         # This is a helper field for trianglulation
         self.opposite = None
 
+@jit(nopython=True)
+def numba_push(ax, ay, bx, by, max_dist, scalar):
+    dx = ax - bx
+    dy = ay - by
+    
+    d2 = (dx*dx) + (dy*dy)
+
+    if d2 > max_dist ** 2:
+        return (0, 0)
+
+    d = math.sqrt(d2)
+    dxn = dx / d
+    dyn = dy / d
+
+    mag = ((1 / (d*d)) * scalar)
+    return (dxn * mag, dyn * mag)
 
 class Node:
-    NEIGHBOR_DISTANCE = 5
+    NEIGHBOR_DISTANCE = 3
     NEIGHBOR_PUSH_SCALAR = 0.05
-    OTHER_PUSH_SCALAR = 10
+    OTHER_PUSH_SCALAR = 8
+    OTHER_RING_PUSH_SCALAR = 3
     MAX_DIST_OTHER = NEIGHBOR_DISTANCE * 5
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, ring_index):
         global _node_id
         self.pos = Vec3(x, y)
         self.pos_next = Vec3(self.pos)
         self.prev = None
         self.next = None
+        self.ring_index = ring_index
 
         self.id = _node_id
         _node_id += 1
@@ -44,33 +65,43 @@ class Node:
             self.next = node
             node.set_prev(self)
 
-    def update(self, nodes):
+    def update(self, rings):
         # Maintain distance from neighbors
         self.pos_next.set(self.pos)
 
         self.neighbor_push(self.prev)
         self.neighbor_push(self.next)
 
-        for node in nodes:
-            if node in (self.next, self.prev, self):
-                continue
+        for ring_index, nodes in enumerate(rings):
+            if self.ring_index == ring_index:
+                scalar = Node.OTHER_PUSH_SCALAR
+            else:
+                scalar = Node.OTHER_RING_PUSH_SCALAR
+            for node in nodes:
+                if node in (self.next, self.prev, self):
+                    continue
 
-            self.other_push(node)
+                self.other_push(node, scalar)
+       
 
-    def other_push(self, node):
-        vec = self.pos - node.pos
-        distance2 = vec.length2()
+    def other_push(self, node, scalar):
+        dx, dy = numba_push(self.pos.x, self.pos.y, node.pos.x, node.pos.y, self.MAX_DIST_OTHER, scalar)
+        self.pos_next.x += dx
+        self.pos_next.y += dy
 
-        if distance2 > self.MAX_DIST_OTHER ** 2:
-            return
+        # vec = self.pos - node.pos
+        # distance2 = vec.length2()
 
-        distance = math.sqrt(distance2)
-        vec_norm = Vec3(vec)
-        vec_norm /= distance
+        # if distance2 > self.MAX_DIST_OTHER ** 2:
+        #     return
 
-        push_force = vec_norm * ((1 / distance) * self.OTHER_PUSH_SCALAR)
+        # distance = math.sqrt(distance2)
+        # vec_norm = Vec3(vec)
+        # vec_norm /= distance
 
-        self.pos_next += push_force
+        # push_force = vec_norm * ((1 / (distance*distance)) * scalar)
+
+        # self.pos_next += push_force
 
     def neighbor_push(self, node):
         vec = self.pos - node.pos
@@ -89,50 +120,61 @@ class Node:
             self.id, self.prev.id, self.next.id
         )
 
+RingDefinition = namedtuple('RingDefinition', ['radius', 'growth_rate'])
 
 class DiffLine:
     def __init__(self):
-        self.root = None
-        self.nodes = []
+        self.roots = []
+        self.rings = []
         random.seed(0)
 
     def init_circle(self):
-        radius = 12
-        num_points = math.floor((radius * math.pi * 2) / Node.NEIGHBOR_DISTANCE)
+        self.ring_defs = [
+            RingDefinition(4, .9),
+            RingDefinition(8, .5),
+        ]
 
-        self.root = Node(radius, 0)
-        self.nodes.append(self.root)
-        previous = self.root
-        for i in range(1, num_points):
-            a = (i / num_points) * math.pi * 2
-            node = Node(math.cos(a) * radius, math.sin(a) * radius)
-            self.nodes.append(node)
-            previous.set_next(node)
-            previous = node
-        previous.set_next(self.root)
+        for ring_index, ring_def in enumerate(self.ring_defs):
+            num_points = math.floor((ring_def.radius * math.pi * 2) / Node.NEIGHBOR_DISTANCE)
+
+            root = Node(ring_def.radius, 0, ring_index)
+            self.roots.append(root)
+            nodes = []
+            nodes.append(root)
+            previous = root
+            for i in range(1, num_points):
+                a = (i / num_points) * math.pi * 2
+                node = Node(math.cos(a) * ring_def.radius, math.sin(a) * ring_def.radius, ring_index)
+                nodes.append(node)
+                previous.set_next(node)
+                previous = node
+            previous.set_next(root)
+            self.rings.append(nodes)
 
     def insert_node(self):
-        insert_index = random.randint(1, len(self.nodes) - 1)
-        prev = self.nodes[insert_index]
-        next = prev.next
+        for ring_index, nodes in enumerate(self.rings):
+            ring_def = self.ring_defs[ring_index]
+            if random.random() < ring_def.growth_rate:
+                insert_index = random.randint(1, len(nodes) - 1)
+                prev = nodes[insert_index]
+                next = prev.next
 
-        node = Node((prev.pos.x + next.pos.x) * 0.5, (prev.pos.y + next.pos.y) * 0.5)
-        self.nodes.insert(insert_index + 1, node)
-        prev.set_next(node)
-        next.set_prev(node)
+                node = Node((prev.pos.x + next.pos.x) * 0.5, (prev.pos.y + next.pos.y) * 0.5, ring_index)
+                nodes.insert(insert_index + 1, node)
+                prev.set_next(node)
+                next.set_prev(node)
 
     def update(self):
         self.insert_node()
 
-        for i, node in enumerate(self.nodes):
-            if self.nodes[(i + 1) % len(self.nodes)] != node.next:
-                raise Exception
+        # for i, node in enumerate(self.nodes):
+        #     if self.nodes[(i + 1) % len(self.nodes)] != node.next:
+        #         raise Exception
 
-        for node in self.nodes:
-            node.update(self.nodes)
+        for nodes in self.rings:
+            for node in nodes:
+                node.update(self.rings)
 
-        for node in self.nodes:
-            node.pos = node.pos_next
-
-    def get_nodes(self):
-        return self.nodes
+        for nodes in self.rings:
+            for node in nodes:
+                node.pos = node.pos_next
--- dan/project/slicestack/run_sim.py
+++ dan/project/slicestack/run_sim.py
@@ -9,26 +9,6 @@ from p5 import *
 
 dl = DiffLine()
 
-def shrink_layer(layer, offset):
-    z = layer[0].z
-
-    pco = pyclipper.PyclipperOffset()
-    pco.AddPath(
-        [(p.x, p.y) for p in layer],
-        pyclipper.JT_SQUARE,
-        pyclipper.ET_CLOSEDPOLYGON
-    )
-
-    solution = pco.Execute(-offset)
-
-    if len(solution) > 1:
-        print( "warning, solution len: {}".format(len(solution)))
-
-    # Only use the longest solution, if there's more than one we're probably fucked anyhow
-    path = max(solution, key=lambda d: len(d))
-
-    return [Vec3(p[0], p[1], z) for p in path]
-
 def setup():
     size(800, 600)
     # no_stroke()
@@ -36,29 +16,27 @@ def setup():
     dl.init_circle()
 
 
+
 def draw():
+    dl.update()
     dl.update()
 
     background(255)
+    
 
     push_matrix()
     translate(width * 0.5, height * 0.5)
 
-    nodes = dl.get_nodes()
-
-    points = [n.pos for n in nodes]
-
-    shrunk_points = shrink_layer(points, 2)
-
-    for i, p in enumerate(points):
-        next = points[(i + 1) % len(points)]
-        line((p.x, p.y), (next.x, next.y))
+    for nodes in dl.rings:
+        for i, n in enumerate(nodes):
+            line((n.pos.x, n.pos.y), (n.next.pos.x, n.next.pos.y))
     
-    for i, p in enumerate(shrunk_points):
-        next = shrunk_points[(i + 1) % len(shrunk_points)]
-        line((p.x, p.y), (next.x, next.y))
+    # for i, p in enumerate(shrunk_points):
+    #     next = shrunk_points[(i + 1) % len(shrunk_points)]
+    #     line((p.x, p.y), (next.x, next.y))
 
     reset_matrix()
+
 
 
 run()
